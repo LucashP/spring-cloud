c2370ec8e41840fa21526c1a917fa1227691e192
LucashP <pula.lukasz@gmail.com>
2017-01-27 00:00:11 +0100

    repository for reservation

--- reservation-service/src/main/java/com/example/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/example/ReservationServiceApplication.java
@@ -1,12 +1,68 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Collection;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class ReservationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReservationServiceApplication.class, args);
+	}
+}
+
+@Component
+class SampleDataCLR implements CommandLineRunner {
+
+	private final ReservationRepository reservationRepository;
+
+	@Autowired
+	public SampleDataCLR(ReservationRepository reservationRepository) {
+		this.reservationRepository = reservationRepository;
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+		Stream.of("Josh", "Niyas", "Arand", "Lun", "Huiren", "Aditi", "Bertina", "Edward", "Andre", "Shaun").forEach(name -> reservationRepository.save(new Reservation(name)));
+		reservationRepository.findAll().stream().forEach(System.out::println) ;
+	}
+}
+
+interface ReservationRepository extends JpaRepository<Reservation, Long> {
+	Collection<Reservation> findByReservationName(String rn);
+}
+
+@Entity
+class Reservation {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String reservationName;
+
+	public Reservation() {
+	}
+
+	public Reservation(String reservationName) {
+		this.reservationName = reservationName;
+	}
+
+	@Override
+	public String toString() {
+		return "Reservation{" +
+				"id=" + id +
+				", reservationName='" + reservationName + '\'' +
+				'}';
 	}
 }
