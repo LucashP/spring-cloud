7e9e5ad1551ffea8d2b3a2809533fb3f8d0bfd41
LucashP <pula.lukasz@gmail.com>
2017-02-03 00:25:14 +0100

    Read message from config server. application.properties not override config server properties.

--- reservation-service/src/main/java/com/example/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/example/ReservationServiceApplication.java
@@ -1,16 +1,20 @@
 package com.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -42,6 +46,23 @@ class SampleDataCLR implements CommandLineRunner {
 		reservationRepository.findAll().forEach(System.out::println);
 	}
 }
+
+@RestController
+@RefreshScope
+class MessageRestController {
+
+	private final String value;
+
+	public MessageRestController(@Value("${message}") String value) {
+		this.value = value;
+	}
+
+	@GetMapping(value = "/message")
+	public String read() {
+		return this.value;
+	}
+}
+
 
 @Component
 class CustomHealthIndicator implements HealthIndicator {
