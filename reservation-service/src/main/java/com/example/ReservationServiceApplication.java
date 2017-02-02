2ad26be322ebc045bed2e674e6047c768e7baf5b
LucashP <pula.lukasz@gmail.com>
2017-02-03 00:54:25 +0100

    Turn on eureka client in reservation service

--- reservation-service/src/main/java/com/example/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/example/ReservationServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
@@ -22,6 +23,7 @@ import javax.persistence.Id;
 import java.util.Collection;
 import java.util.stream.Stream;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ReservationServiceApplication {
 
