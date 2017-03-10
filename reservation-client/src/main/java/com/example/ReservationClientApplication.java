e1f2b0347fe412b1c77d8afd0839f2f36a573c68
LucashP <pula.lukasz@gmail.com>
2017-03-10 23:23:49 +0100

    HystrixCommad and CircuitBreaker

--- reservation-client/src/main/java/com/example/ReservationClientApplication.java
+++ reservation-client/src/main/java/com/example/ReservationClientApplication.java
@@ -1,8 +1,10 @@
 package com.example;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -16,9 +18,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
+@EnableCircuitBreaker
 @EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -47,6 +51,11 @@ class ReservationApiGatewayRestController {
         this.restTemplate = restTemplate;
     }
 
+    public Collection<String> fallback() {
+        return new ArrayList<>();
+    }
+
+    @HystrixCommand(fallbackMethod = "fallback")
     @RequestMapping(method = RequestMethod.GET, value = "/names")
     public Collection<String> names() {
 
